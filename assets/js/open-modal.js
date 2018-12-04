03e2c9c4a7b78447b5bbf1cf8deb1ecfac02aaa8
luis piña <luispia1290@gmail.com>
2018-12-04 17:29:02 -0600

    coloco descripcion de plantillas en ventana modal

--- assets/js/open-modal.js
+++ assets/js/open-modal.js
@@ -4,31 +4,48 @@ $(function () {
         switch (id) {
             case 'modal1':
                 $(".modal-header").html(
-                    '<h5 class="modal-title" id="exampleModalLabel">Rock black and Green</h5>' +
+                    '<h3 class="modal-title" id="exampleModalLabel">Rock black and Green</h3>' +
                     '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
                     ' <span aria-hidden="true">&times;</span> </button>');
                 $('#img-modal').html('<img src="assets/img/plantillas/rock.png" alt="">');
+
+                $('#desc-modal').html('<h4>Descripcion de la plantilla</h4>' +
+                    '<p></p>');
                 break;
             case 'modal2':
                 $(".modal-header").html(
-                    '<h5 class="modal-title" id="exampleModalLabel">Tacos magic</h5>' +
+                    '<h3 class="modal-title" id="exampleModalLabel">Tacos magic</h3>' +
                     '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
                     '<span aria-hidden="true">&times;</span> </button>');
+
                 $('#img-modal').html('<img src="assets/img/plantillas/template-tacos.png" alt="">');
+
+                $('#desc-modal').html('<h4>Promociona tu servicios y tus mejores platillos </h4>' +
+                    '<p>Con esta plantilla genera nuevos comensales potenciales ' +
+                    ', Atraves de imagenes de tus platillos, cuentales la historia y calidad de tus productos,' +
+                    ' ofrece servicio a domicilio e indica tu ubicacion atraves de google maps' +
+                    '</p>');
                 break;
             case 'modal3':
                 $(".modal-header").html(
-                    '<h5 class="modal-title" id="exampleModalLabel">TED developers</h5>' +
+                    '<h3 class="modal-title" id="exampleModalLabel">TED developers</h3>' +
                     '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
                     '<span aria-hidden="true">&times;</span> </button>');
                 $('#img-modal').html('<img src="assets/img/plantillas/template-ted.png" alt="">');
+
+                $('#desc-modal').html('<h4>Pagina Generada por TED</h4>' +
+                    '<p>Plantilla llamativa que ofrece un contacto con clientes potenciales' +
+                    ' atraves de tu correo electronico, describe que ofreces y que te hace diferente a los demas' +
+                    ' promociona a tu equipo de trabajo, ademas de mostrar que cuentas con los mejores</p>');
                 break;
             case 'modal4':
                 $(".modal-header").html(
-                    '<h5 class="modal-title" id="exampleModalLabel">Refrigeration blueSky</h5>' +
+                    '<h3 class="modal-title" id="exampleModalLabel">Refrigeration blueSky</h3>' +
                     '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
                     '<span aria-hidden="true">&times;</span> </button>');
                 $('#img-modal').html('<img src="assets/img/plantillas/template-tacos.png" alt="">');
+                $('#desc-modal').html('<h4>Climatiza tus ideas</h4>' +
+                    '<p></p>');
                 break;
             default:
                 break;
